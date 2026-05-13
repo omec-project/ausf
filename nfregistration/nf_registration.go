958347a0df05c5f0121bf61cfefbcd45763bbad2
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2026-05-12 19:35:24 -0700

    Update path as per 3GPP specs and fix issue in NRF component
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- nfregistration/nf_registration.go
+++ nfregistration/nf_registration.go
@@ -104,7 +104,7 @@ func heartbeatNF(plmnConfig []models.PlmnId) {
 	patchItem := []models.PatchItem{
 		{
 			Op:    models.PATCHOPERATION_REPLACE,
-			Path:  "/nfstatus",
+			Path:  "/nfStatus",
 			Value: models.NFSTATUS_REGISTERED,
 		},
 	}
